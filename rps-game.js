693c23ffe348af6841ba49991cba0a6854f7a467
Scott L <sluszczewski@gmail.com>
2022-06-08 11:15:55 -0700

    Added rounds, score.  Updated outcome text.

--- rps-game.js
+++ rps-game.js
@@ -37,20 +37,21 @@ function validatePlayerPlay(playerSelection){
         default:
             //console.log("Invalid Choice");
             let badChoice = "invalid"
-            return badChoice
+            return badChoice;
             
     }
 }
 
 function updateScore(player){
-    if(player === "cpu"){
-        cpuScore += 1;
-        return;
+
+    if(player === "cpu"){        
+        cpuScore += 1;        
     }
 
     if(player === "player"){
-        playerScore += 1;
-    }
+        playerScore += 1;              
+    }  
+
 }
 
 function playRound (playerSelection, computerSelection){
@@ -64,44 +65,81 @@ function playRound (playerSelection, computerSelection){
         
         if(computerSelection === "paper"){
             //cpu wins
-            winner = "cpu"
+            console.log("You Lose! Paper beats Rock.")
+            winner = "cpu";
             
         }else if(computerSelection === "scissors"){
             //player wins
-            winner = "player"
+            console.log("You Win! Rock beats Scissors.")
+            winner = "player";
         }
 
     }else if(playerSelection === "paper"){
         
         if(computerSelection === "scissors"){
             //cpu wins
-            winner = "cpu"
+            console.log("You Lose! Scissors beats Paper.")
+            winner = "cpu";
         }else if(computerSelection === "rock"){
             //player wins
-            winner = "player"
+            console.log("You Win! Paper beats Rock.")
+            winner = "player";
         }
 
     }else if(playerSelection === "scissors"){
         
         if(computerSelection === "rock"){
             //cpu wins
-            winner = "cpu"
+            console.log("You Lose! Rock beats Scissors.")
+            winner = "cpu";
         }else if(computerSelection === "paper"){
             //player wins
-            winner = "player"
+            console.log("You Win! Scissors beats Paper.")
+            winner = "player";
         }
     }    
-    console.log(winner)
-    //return winner;
+    
+    return winner;
+}
+
+function game(){
+    const computerSelection = computerPlay();
+    const playerSelection = playerPlay();
+    console.log("Round Number: " + round);
+    
+    let winner = playRound(playerSelection, computerSelection);
+    
+    updateScore(winner);
+    console.log("Your Score: " + playerScore + " CPU Score: " + cpuScore )
+
+    if(cpuScore == 5){
+        console.log("Game over, the cpu has won 5 rounds.");
+        gameOn = false;
+    }
+    if(playerScore == 5){
+        console.log("Congratulations, you won 5 rounds!");
+        gameOn = false;
+    }
+
+    round += 1;
+
 }
 
 //console.log (computerPlay())
  
-const computerSelection = computerPlay();
-const playerSelection = playerPlay();
+
+var gameOn = true;
 var cpuScore = 0;
 var playerScore = 0;
+var round = 1;
+
+
+while(gameOn == true){
+    game();
+}
 
 //alert (`${playerSelection}`);
 
-playRound(playerSelection, computerSelection);
+
+
+
